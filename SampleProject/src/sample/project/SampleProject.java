46530ec1671cfcb84e11cac8bd9e44a25bff9241
kajankajan <30923941+kajankajan@users.noreply.github.com>
2017-08-11 14:56:41 +0530

    Update SampleProject.java

--- SampleProject/src/sample/project/SampleProject.java
+++ SampleProject/src/sample/project/SampleProject.java
@@ -17,7 +17,7 @@ public class SampleProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("chane hear!");
+        System.out.println("i am kajan!");
     }
     
 }
